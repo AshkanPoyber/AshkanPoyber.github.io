9e9bc62996938e5c43b3860baef2e72bef002c1c
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-05-23 10:58:48 +0330

    updated

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
-/*================================ Toggle Icon Navbar =====================*/ 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('navbar');
+/*================================ Toggle Icon Navbar =====================*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector("navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
- 
- /*================================ Scroll Sections Active link =====================*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
 
+/*================================ Scroll Sections Active link =====================*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 
-    /*================================ Sticky Navbar =====================*/
-     let  header = document.querySelector('header');
+  /*================================ Sticky Navbar =====================*/
+  let header = document.querySelector("header");
 
-     header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    //  /*================================ Remove Toggle Icon And Navbar When Click Navbar Link ( Scroll ) =====================*/
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  //  /*================================ Remove Toggle Icon And Navbar When Click Navbar Link ( Scroll ) =====================*/
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
- /*================================ Scroll Reveal =====================*/
- ScrollReveal({ 
-     reset: true,
-     distance: '80px',
-     duration: 2000,
-     delay: 200
+/*================================ Scroll Reveal =====================*/
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
-
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 /*================================ Typed JS =====================*/
-const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'YouTuber', 'Designer'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Software Enginner", "Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
