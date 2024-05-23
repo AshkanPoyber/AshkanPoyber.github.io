d18adde6445d1d15dc7a2bc75562398025050bd9
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-05-23 10:57:31 +0330

    updated

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 /*================================ Typed JS =====================*/
-const Typed = new Typed('.multiple-text', {
+const typed = new Typed('.multiple-text', {
     strings: ['Frontend Developer', 'YouTuber', 'Designer'],
     typeSpeed: 100,
     backSpeed: 100,
