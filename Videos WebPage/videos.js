06885be738eff6b7b701023f6e18cbce40bd59b0
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-06-28 16:24:22 +0330

    .

--- Videos WebPage/videos.js
+++ Videos WebPage/videos.js
@@ -1,22 +1,22 @@
 // script.js
 const videos = {
   temp1: [
-    { src: "video1.mp4", thumb: "thumb1.jpg", title: "ویدئو 1" },
-    { src: "video2.mp4", thumb: "thumb2.jpg", title: "ویدئو 2" },
+    { src: "video1.mp4", thumb: "thumb1.jpg", title: "First Video" },
+    { src: "video2.mp4", thumb: "thumb2.jpg", title: "Second Video" },
   ],
   temp2: [
-    { src: "video3.mp4", thumb: "thumb3.jpg", title: "ویدئو 3" },
-    { src: "video4.mp4", thumb: "thumb4.jpg", title: "ویدئو 4" },
+    { src: "video3.mp4", thumb: "thumb3.jpg", title: "First Video" },
+    { src: "video4.mp4", thumb: "thumb4.jpg", title: "Second Video" },
   ],
   temp3: [
-    { src: "video1.mp4", thumb: "thumb1.jpg", title: "ویدئو 1" },
-    { src: "video2.mp4", thumb: "thumb2.jpg", title: "ویدئو 2" },
+    { src: "video1.mp4", thumb: "thumb1.jpg", title: "First Video" },
+    { src: "video2.mp4", thumb: "thumb2.jpg", title: "Second Video" },
   ],
   temp4: [
-    { src: "video3.mp4", thumb: "thumb3.jpg", title: "ویدئو 3" },
-    { src: "video4.mp4", thumb: "thumb4.jpg", title: "ویدئو 4" },
+    { src: "video3.mp4", thumb: "thumb3.jpg", title: "First Video" },
+    { src: "video4.mp4", thumb: "thumb4.jpg", title: "Second Video" },
   ],
-  // دسته‌بندی‌های دیگر
+  // Another Category
 };
 
 function filterVideos(category) {
