b27c658fb438ed99130ed5bdef44aad3d10a2621
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-06-29 11:30:09 +0330

    .

--- Videos WebPage/videos.js
+++ Videos WebPage/videos.js
@@ -11,6 +11,12 @@ const videos = {
     { src: "video3.mp4", thumb: "Thumbnails/Temp2.png", title: "First Video" },
     { src: "video4.mp4", thumb: "Thumbnails/Temp1.png", title: "Second Video" },
   ],
+  another: [
+    { src: "video3.mp4", thumb: "Thumbnails/Temp2.png", title: "First Video" },
+    { src: "video4.mp4", thumb: "Thumbnails/Temp1.png", title: "Second Video" },
+    { src: "video4.mp4", thumb: "Thumbnails/Temp1.png", title: "Second Video" },
+    { src: "video4.mp4", thumb: "Thumbnails/Temp1.png", title: "Second Video" },
+  ],
   // Another Category
 };
 
