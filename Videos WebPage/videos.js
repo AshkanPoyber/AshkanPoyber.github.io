c510d7ce3f1388bbce1dc7d486a1a7ff14208a6c
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-06-29 10:13:41 +0330

    .

--- Videos WebPage/videos.js
+++ Videos WebPage/videos.js
@@ -1,44 +1,44 @@
-// script.js
-const videos = {
-  motivationals: [
-    {
-      src: "Videos/Temp1.mp4",
-      thumb: "Thumbnails/Temp1.png",
-      title: "First Video",
-    },
-    { src: "video2.mp4", thumb: "Thumbnails/Temp2.png", title: "Second Video" },
-  ],
-  projects: [
-    { src: "video3.mp4", thumb: "Thumbnails/Temp2.png", title: "First Video" },
-    { src: "video4.mp4", thumb: "Thumbnails/Temp1.png", title: "Second Video" },
-  ],
-  // Another Category
-};
+// // script.js
+// const videos = {
+//   motivationals: [
+//     {
+//       src: "Videos/Temp1.mp4",
+//       thumb: "Thumbnails/Temp1.png",
+//       title: "First Video",
+//     },
+//     { src: "video2.mp4", thumb: "Thumbnails/Temp2.png", title: "Second Video" },
+//   ],
+//   projects: [
+//     { src: "video3.mp4", thumb: "Thumbnails/Temp2.png", title: "First Video" },
+//     { src: "video4.mp4", thumb: "Thumbnails/Temp1.png", title: "Second Video" },
+//   ],
+//   // Another Category
+// };
 
-function filterVideos(category) {
-  const videoList = document.getElementById("videoList");
-  videoList.innerHTML = "";
-  videos[category].forEach((video) => {
-    const videoItem = document.createElement("div");
-    videoItem.classList.add("video-item");
-    videoItem.innerHTML = `
-            <img src="${video.thumb}" alt="${video.title}" onclick="playVideo('${video.src}')">
-            <p>${video.title}</p>
-        `;
-    videoList.appendChild(videoItem);
-  });
-}
+// function filterVideos(category) {
+//   const videoList = document.getElementById("videoList");
+//   videoList.innerHTML = "";
+//   videos[category].forEach((video) => {
+//     const videoItem = document.createElement("div");
+//     videoItem.classList.add("video-item");
+//     videoItem.innerHTML = `
+//             <img src="${video.thumb}" alt="${video.title}" onclick="playVideo('${video.src}')">
+//             <p>${video.title}</p>
+//         `;
+//     videoList.appendChild(videoItem);
+//   });
+// }
 
-function playVideo(src) {
-  const video = document.getElementById("video");
-  const videoSource = document.getElementById("videoSource");
-  videoSource.src = src;
-  video.load();
-  video.play();
-}
+// function playVideo(src) {
+//   const video = document.getElementById("video");
+//   const videoSource = document.getElementById("videoSource");
+//   videoSource.src = src;
+//   video.load();
+//   video.play();
+// }
 
-function changePlaybackRate() {
-  const video = document.getElementById("video");
-  const playbackRate = document.getElementById("playbackRate").value;
-  video.playbackRate = playbackRate;
-}
+// function changePlaybackRate() {
+//   const video = document.getElementById("video");
+//   const playbackRate = document.getElementById("playbackRate").value;
+//   video.playbackRate = playbackRate;
+// }
